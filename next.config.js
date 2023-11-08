9dfbc62bba9b55d731c648c2e0f179588b8d1348
Jeffry <jbucht7@gmail.com>
2023-11-07 19:14:33 -0600

    lol

--- next.config.js
+++ next.config.js
@@ -4,7 +4,4 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com", "i.pinimg.com", "scontent.fgua2-2.fna.fbcdn.net" , "scontent.fgua2-1.fna.fbcdn.net"],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
 };
