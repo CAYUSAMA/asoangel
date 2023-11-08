d279dc735eb58151a0bb850c5236126eb0d2d577
Jeffry <jbucht7@gmail.com>
2023-11-07 18:57:55 -0600

    ingorebuilderrors

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com", "i.pinimg.com", "scontent.fgua2-2.fna.fbcdn.net" , "scontent.fgua2-1.fna.fbcdn.net"],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
